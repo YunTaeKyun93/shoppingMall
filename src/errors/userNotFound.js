7b3f39ffbc4cf15aec4ce9674ef3e7ccf848b160
YunTaeKyun93 <98891717+YunTaeKyun93@users.noreply.github.com>
2022-12-10 13:47:15 +0900

    1210

--- src/errors/userNotFound.js
+++ src/errors/userNotFound.js
@@ -1,5 +1,5 @@
-class userNotFound extends Error() {
-  constructor(userId) {
+class userNotFound extends Error {
+  constructor({userId}) {
     super(`해당하는 유저ID를 가진 유저를 찾을 수 없습니다${userId}`);
     this.userId = userId;
   }
